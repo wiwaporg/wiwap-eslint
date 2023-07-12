70b4e4f006cbbca91fbb6db218439878b6b39ae4
gustavocotalima <gustavolima@unifei.edu.br>
2023-07-12 01:57:19 -0300

    testing next

--- next.js
+++ next.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    //'plugin:react-hooks/recommended',
     'standard',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
